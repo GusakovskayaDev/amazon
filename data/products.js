1db8e5930487213857cb01964102a5b13ec1aed7
PoniaPolla <gusakovskayadev@gmail.com>
2024-11-29 00:03:16 +0300

    fixed problems

--- data/products.js
+++ data/products.js
@@ -77,3 +77,54 @@ class Appliance extends Product {
 		`;
 	}
 }
+
+export let products = [];
+
+export function loadProductsFetch() {
+	const promise = fetch('https://supersimplebackend.dev/products') //fetch default has a GET
+	.then((response) => {
+		return response.json();
+	}).then((productsData) => {
+		products = productsData.map((productDetails) => {
+			if (productDetails.type === 'clothing') {
+				return new Clothing(productDetails);
+			} else if (productDetails.type === 'appliance') {
+				return new Appliance(productDetails);
+			}
+			return new Product(productDetails);
+		});
+
+		console.log('load products');
+	}).catch((error) => {
+		console.log('Unexpected error. Please try again later.');
+	});
+
+	return promise;
+}
+
+export function loadProducts(fun) {
+	const xhr = new XMLHttpRequest();
+
+	xhr.addEventListener('load', () => {
+		products = JSON.parse(xhr.response).map((productDetails) => {
+			if (productDetails.type === 'clothing') {
+				return new Clothing(productDetails);
+			} else if (productDetails.type === 'appliance') {
+				return new Appliance(productDetails);
+			}
+			return new Product(productDetails);
+		});
+
+		// console.log('load products');
+
+		fun();
+	});
+
+	xhr.addEventListener('error', (error) => {
+		console.log('Unexpected error. Please try again later.');
+	});
+
+	xhr.open('GET', 'https://supersimplebackend.dev/products');
+	xhr.send();
+}
+loadProducts();
